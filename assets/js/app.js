637d0c386e0e7818f24dc52d36d7ff8040b32b5f
Bladimir Orellana <me@bladimirorellana.com>
2019-08-19 19:04:39 -0500

    I added the footer to all pages using a template on javascript

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,21 @@
 /// <reference path="../typings/globals/jquery/index.d.ts" />im
 import {welcomeMessage} from "./modules/pages/_home.js";
+import {template} from "./modules/footer/_footer.js";
 $(document).ready(function(){
 
 
 const appInit = () => {
+    //WELCOME MESSAGE
     welcomeMessage();
+    //FOOTER
+    $(".footer-container").append(template)
+    //SIDE NAV
     $('.sidenav').sidenav(); 
+    //PARALLAX
     $('.parallax').parallax();
+   //MODAL FOR WHATSAPP
     $('.modal').modal();
+
    
 }
 appInit()
